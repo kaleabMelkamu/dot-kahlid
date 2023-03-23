c79fc0d51a324296d9b4939d5ca75fb943380665
kale <kaleabm18@gmail.com>
2023-03-23 13:12:05 +0300

    hello is added

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -3,10 +3,9 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div>
-       <h2>Hello world</h2>
+       <h2>Helloooo beautiful people!!</h2>
 
-       <h3>Do you know we are??? </h3>
-       <h1>we are Ruth and Kale</h1>
+      
     </div>
   )
 }
