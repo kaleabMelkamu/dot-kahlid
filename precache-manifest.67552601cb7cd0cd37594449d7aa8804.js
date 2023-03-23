5b3ff72eeebc1c80b48c184ffcefdcc75c72e431
kale <kaleabm18@gmail.com>
2023-03-23 12:15:20 +0300

    Updates

---
+++ precache-manifest.67552601cb7cd0cd37594449d7aa8804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c7f4d8ea0da7cedf70f",
+    "url": "/react-gh-pages/static/css/main.22ff98c2.chunk.css"
+  },
+  {
+    "revision": "9c7f4d8ea0da7cedf70f",
+    "url": "/react-gh-pages/static/js/main.9c7f4d8e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/react-gh-pages/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a07bf9ad53fac55d281ece3d8029fa9e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
