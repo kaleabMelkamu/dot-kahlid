a4c0b84ee27f3fa63983f3d6c528f20e858f3ecf
kale <kaleabm18@gmail.com>
2023-03-24 11:52:40 +0300

    side bar content

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -3,9 +3,14 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div>
-       <h2>Helloooo beautiful people!!</h2>
-
-      
+       <h2>Make a list</h2>
+<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+</ul>
+       
     </div>
   )
 }
